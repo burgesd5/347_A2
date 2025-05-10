11339b7dbd07b06afc3f56cbbfd869885688d56c
burgesd5 <burgesd5@wwu.edu>
2025-05-09 20:34:04 -0700

    starting C empty

---
+++ A/Functions.c
@@ -0,0 +1,37 @@
+#pragma warning(disable : 4996)
+//https://www.tutorialspoint.com/c_standard_library/c_function_fgets.htm
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "Song.h"
+
+int writeSongRecord(char* filename, char* name, char* artist, int length)
+{
+    
+    return 0;
+}
+
+int readSongRecords(char* filename)
+{
+    
+
+    return 0;
+}
+
+int totalTimeOfSongs(char* filename)
+{
+    int totalTimeOfAllSongs = 0;
+
+    
+    return totalTimeOfAllSongs;
+}
+
+int findSong(char* filename, char* songName, Song* song)
+{
+    int songFound = 0;
+
+    
+
+    return songFound;
+}
